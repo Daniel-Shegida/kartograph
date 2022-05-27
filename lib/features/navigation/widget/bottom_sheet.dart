ac2e0a242d6f672e3113c6c1b4dd8a1f9ed60068
Даниил Шегида <don.shegida@mail.ru>
2022-05-27 14:57:23 +0300

    feat: refactor of bottom_sheet.dart

--- lib/features/navigation/widget/bottom_sheet.dart
+++ lib/features/navigation/widget/bottom_sheet.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kartograph/assets/colors/colors.dart';
+import 'package:kartograph/assets/res/project_icons.dart';
 import 'package:kartograph/assets/strings/projectStrings.dart';
 import 'package:routemaster/routemaster.dart';
 
@@ -12,56 +13,35 @@ class BottomNavigator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tabPage = TabPage.of(context);
-    final Widget svgIcon = SvgPicture.network(
-      'https://site-that-takes-a-while.com/image.svg',
-      semanticsLabel: 'A shark?!',
-      // placeholderBuilder: (BuildContext context) => Container(
-      //     padding: const EdgeInsets.all(30.0),
-      //     child: const CircularProgressIndicator()),
-    );
 
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: tabPage.index,
-        items: [
+        items: const [
           BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-                "assets/icons/List.svg",
-                color: ProjectColors.mainLightTheme,
+            icon: _BottomIcons(
+              iconAddress: ProjectIcons.list,
             ),
-            // Icon(Icons.file_copy_outlined),
-            activeIcon: SvgPicture.asset(
-                "assets/icons/List Full.svg",
-                color: ProjectColors.mainLightTheme,
+            activeIcon: _BottomIcons(
+              iconAddress: ProjectIcons.listFull,
             ),
-      // Icon(Icons.file_copy),
             label: ProjectStrings.placesBottom,
           ),
           BottomNavigationBarItem(
-            icon: SvgPicture.asset(
-                "assets/icons/Map.svg",
-                color: ProjectColors.mainLightTheme,
-                semanticsLabel: 'A red up arrow'
+            icon: _BottomIcons(
+              iconAddress: ProjectIcons.map,
             ),
-            // Icon(Icons.assignment_turned_in_outlined),
-            activeIcon: SvgPicture.asset(
-                "assets/icons/Map Full.svg",
-                color: ProjectColors.mainLightTheme,
+            activeIcon: _BottomIcons(
+              iconAddress: ProjectIcons.mapFull,
             ),
-            // Icon(Icons.assignment_turned_in),
             label: ProjectStrings.mapBottom,
           ),
           BottomNavigationBarItem(
-            icon:
-            // svgIcon,
-            SvgPicture.asset(
-                  "assets/icons/Settings.svg",
-                  color: ProjectColors.mainLightTheme,
-              ),
-            // Icon(Icons.account_circle_outlined),
-            activeIcon: SvgPicture.asset(
-                "assets/icons/Settings-fill.svg",
-                color: ProjectColors.mainLightTheme,
+            icon: _BottomIcons(
+              iconAddress: ProjectIcons.settings,
+            ),
+            activeIcon: _BottomIcons(
+              iconAddress: ProjectIcons.settingsFull,
             ),
             label: ProjectStrings.settingBottom,
           ),
@@ -76,6 +56,20 @@ class BottomNavigator extends StatelessWidget {
           for (final stack in tabPage.stacks) PageStackNavigator(stack: stack),
         ],
       ),
+    );
+  }
+}
+
+class _BottomIcons extends StatelessWidget {
+  final String iconAddress;
+
+  const _BottomIcons({required this.iconAddress, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SvgPicture.asset(
+      iconAddress,
+      color: ProjectColors.mainLightTheme,
     );
   }
 }
