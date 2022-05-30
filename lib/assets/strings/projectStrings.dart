8e420d8dad148cf64f6c22800ffe141af8a1a950
Даниил Шегида <don.shegida@mail.ru>
2022-05-30 15:16:43 +0300

    fix: убран url из виджета

---
+++ lib/assets/strings/projectStrings.dart
@@ -0,0 +1,14 @@
+/// Strings of Kartograph project
+abstract class ProjectStrings {
+  /// PlacesScreen bottom name.
+  static const String placesBottom = 'places';
+  /// MapScreen bottom name.
+  static const String mapBottom = 'map';
+  /// SettingScreen bottom name.
+  static const String settingBottom = 'setting';
+  /// Сайт откуда берется карта.
+  static const String mapSite = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/';
+  /// Персональный токен.
+  static const String mapToken = '?access_token=pk.eyJ1IjoidGVzdGVyLWRvbm8iLCJhIjoiY2wzbG1idXVuMW9jZzNkcXdnNGZwN2NlOSJ9.LGGfGe5NuuAyD-FFepK47g';
+
+}
