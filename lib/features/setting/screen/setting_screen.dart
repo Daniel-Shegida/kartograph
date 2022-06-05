f2aac7a1d0a0dd8e5a7173654af8afcd758c63d4
Daniel Shegida <don.shegida@mail.ru>
2022-06-05 19:10:11 +0300

    feat: добавление переменных для экрана настроек

--- lib/features/setting/screen/setting_screen.dart
+++ lib/features/setting/screen/setting_screen.dart
@@ -1,11 +1,14 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kartograph/assets/colors/colors.dart';
+import 'package:kartograph/assets/res/project_icons.dart';
 import 'package:kartograph/assets/strings/projectStrings.dart';
 import 'package:kartograph/features/setting/screen/setting_screen_wm.dart';
+import 'package:kartograph/features/setting/widgets/cardWidget.dart';
 
-
+/// экран настроек
 class SettingScreen extends ElementaryWidget<ISettingWidgetModel> {
-
   /// standard consctructor for elem
   const SettingScreen({
     Key? key,
@@ -18,29 +21,72 @@ class SettingScreen extends ElementaryWidget<ISettingWidgetModel> {
       appBar: AppBar(
         title: const Text(ProjectStrings.settings),
       ),
-      body:  Column(
+      body: Column(
         children: [
-          
+          StateNotifierBuilder<bool>(
+            listenableState: wm.themeNotifier,
+            builder: (ctx, value) {
+              return _ThemeChangerCard(
+                cardName: wm.currentTheme,
+                swithFlag: value ?? false,
+                switchFunc: wm.onThemeTap,
+              );
+            },
+          ),
+          _TutorialWatcherCard(
+            helperFunc: wm.onTutorialTap,
+          ),
         ],
       ),
     );
   }
 }
 
-class ThemeChangerCard extends StatelessWidget {
-  const ThemeChangerCard({Key? key}) : super(key: key);
+class _ThemeChangerCard extends StatelessWidget {
+  /// текущая тема
+  final String cardName;
+
+  /// текущий флаг темы
+  final bool swithFlag;
+
+  /// функция переключающая тему
+  final Function(bool) switchFunc;
+
+  const _ThemeChangerCard(
+      {required this.cardName,
+      required this.swithFlag,
+      required this.switchFunc,
+      Key? key,})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CardWidget.withSwitch(
+      cardName: cardName,
+      onPressed: () {},
+      switchFlag: swithFlag,
+      switchFun: switchFunc,
+    );
   }
 }
 
-class TutorialWatcherCard extends StatelessWidget {
-  const TutorialWatcherCard({Key? key}) : super(key: key);
+class _TutorialWatcherCard extends StatelessWidget {
+  /// функция которая активируется при нажатии на иконку
+  final Function() helperFunc;
+
+  const _TutorialWatcherCard({required this.helperFunc, Key? key})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return CardWidget.withHelper(
+      cardName: ProjectStrings.tutorialCard,
+      onPressed: () {},
+      icon: SvgPicture.asset(
+        ProjectIcons.list,
+        color: ProjectColors.mainLightTheme,
+      ),
+      onHelper: helperFunc,
+    );
   }
 }
--- lib/features/setting/screen/setting_screen_wm.dart
+++ lib/features/setting/screen/setting_screen_wm.dart
@@ -1,8 +1,8 @@
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:kartograph/assets/strings/projectStrings.dart';
 import 'package:kartograph/features/setting/screen/setting_screen.dart';
 import 'package:kartograph/features/setting/screen/setting_screen_model.dart';
-
 
 /// Builder for [SettingWidgetModel]
 SettingWidgetModel settingWidgetModelFactory(BuildContext context) {
@@ -10,14 +10,25 @@ SettingWidgetModel settingWidgetModelFactory(BuildContext context) {
 }
 
 /// WidgetModel for [SettingScreen]
-class SettingWidgetModel extends WidgetModel<SettingScreen, SettingModel> with SingleTickerProviderWidgetModelMixin
-    implements ISettingWidgetModel  {
+class SettingWidgetModel extends WidgetModel<SettingScreen, SettingModel>
+    with SingleTickerProviderWidgetModelMixin
+    implements ISettingWidgetModel {
+  final _themeNotifier = StateNotifier<bool>();
+
+  @override
+  String get currentTheme => _currentTheme;
+
+  @override
+  StateNotifier<bool> get themeNotifier => _themeNotifier;
+
+  String _currentTheme = ProjectStrings.lightTheme;
 
   /// standard consctructor for elem
   SettingWidgetModel(SettingModel model) : super(model);
 
   @override
   void initWidgetModel() {
+    _themeNotifier.accept(false);
     super.initWidgetModel();
   }
 
@@ -25,8 +36,33 @@ class SettingWidgetModel extends WidgetModel<SettingScreen, SettingModel> with S
   void dispose() {
     super.dispose();
   }
+
+  @override
+  void onThemeTap(bool switchFlag) {
+    _themeNotifier.accept(switchFlag);
+    if (_themeNotifier.value ?? false) {
+      _currentTheme = ProjectStrings.darkTheme;
+    } else {
+      _currentTheme = ProjectStrings.lightTheme;
+    }
+  }
+
+  @override
+  void onTutorialTap() {}
 }
 
 /// Interface of [SettingWidgetModel].
 abstract class ISettingWidgetModel extends IWidgetModel {
+  /// уведомитель флага темы
+  StateNotifier<bool> get themeNotifier;
+
+  /// текущая тема
+  String get currentTheme;
+
+  /// метод свитчера карточки темы
+  // ignore: avoid_positional_boolean_parameters
+  void onThemeTap(bool switchFlag);
+
+  /// метод карточки туториала
+  void onTutorialTap();
 }
