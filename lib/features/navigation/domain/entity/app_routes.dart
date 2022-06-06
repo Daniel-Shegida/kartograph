e563b0d087008ca02b221977cb8e6db219519db5
Даниил Шегида <don.shegida@mail.ru>
2022-06-06 12:33:43 +0300

    feat: промежуточный результат экрана добавления места

--- lib/features/navigation/domain/entity/app_routes.dart
+++ lib/features/navigation/domain/entity/app_routes.dart
@@ -2,6 +2,7 @@
 import 'package:kartograph/features/map/map_route.dart';
 import 'package:kartograph/features/navigation/domain/entity/app_route_paths.dart';
 import 'package:kartograph/features/navigation/widget/bottom_sheet.dart';
+import 'package:kartograph/features/place_adding/place_add_route.dart';
 import 'package:kartograph/features/places/place_route.dart';
 import 'package:kartograph/features/setting/setting_route.dart';
 import 'package:routemaster/routemaster.dart';
@@ -10,8 +11,10 @@ import 'package:routemaster/routemaster.dart';
 final routes = RouteMap(
   routes: {
     /// 'путь': (_) => MaterialPage(child: виджет),
-    '/': (_) =>
-    const Redirect('${AppRoutePaths.tabs}${AppRoutePaths.placesScreen}'),
+    // '/': (_) =>
+    // const Redirect('${AppRoutePaths.tabs}${AppRoutePaths.placesScreen}')
+'/': (_) =>
+const Redirect('SettingScreenRoute'),
 
     '/tabs': (route) => const TabPage(
       child: BottomNavigator(),
@@ -29,5 +32,6 @@ final routes = RouteMap(
 
     '${AppRoutePaths.tabs}${AppRoutePaths.settingScreen}': (_) =>
     const SettingScreenRoute(),
+    'SettingScreenRoute' : (_) => PlaceAddScreenRoute()
   },
 );
--- lib/features/place_adding/screen/place_add_screen.dart
+++ lib/features/place_adding/screen/place_add_screen.dart
@@ -1,45 +1,181 @@
-
 import 'package:elementary/elementary.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:kartograph/assets/colors/colors.dart';
+import 'package:kartograph/assets/res/project_icons.dart';
+import 'package:kartograph/assets/strings/projectStrings.dart';
 import 'package:kartograph/features/place_adding/screen/place_add_screen_wm.dart';
 
 class PlaceAddingScreen extends ElementaryWidget<IPlaceAddingWidgetModel> {
-
   /// standard consctructor for elem
   const PlaceAddingScreen({
     Key? key,
     WidgetModelFactory wmFactory = placeAddingWidgetModelFactory,
   }) : super(wmFactory, key: key);
 
+  final String test = 'One';
+
   @override
   Widget build(IPlaceAddingWidgetModel wm) {
     return Scaffold(
       appBar: AppBar(
-        leading: TextButton(onPressed: () {  }, child: Text("da"),),
+        leading: TextButton(
+          onPressed: () {},
+          child: const Text("da"),
+        ),
       ),
-      body:  Column(
-        children: [
-          Text("place"),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 15.5),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _PlaceTitles(
+              titleText: ProjectStrings.category,
+            ),
+            DropdownButton<String>(
+              value: test,
+              icon: SvgPicture.asset(
+                ProjectIcons.view,
+                color: ProjectColors.mainLightTheme,
+              ),
+              isExpanded: true,
+              elevation: 16,
+              style: const TextStyle(color: Colors.deepPurple),
+              underline: Container(
+                height: 2,
+                color: Colors.deepPurpleAccent,
+              ),
+              onChanged: (String? newValue) {},
+              items: <String>['One', 'Two', 'Free', 'Four']
+                  .map<DropdownMenuItem<String>>((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+            ),
+            _PlaceTitles(
+              titleText: ProjectStrings.name,
+            ),
+            _textInput(),
+            _PlaceTitles(
+              titleText: ProjectStrings.description,
+            ),
+            _textInput(lines: 4,),
+            _coordinates(),
+            TextButton(
+              onPressed: () {},
+              child: Text(
+                ProjectStrings.show,
+                style: TextStyle(
+                  color: ProjectColors.mainLightTheme,
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
-            child: TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                hintText: 'Enter a search term',
-              ),
-            ),
-          ),
-        ],
+            Expanded(child: Container()),
+            _AddPlaceButton(),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class _PlaceTitles extends StatelessWidget {
+  final String titleText;
+
+  const _PlaceTitles({required this.titleText, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      titleText,
+      style: const TextStyle(color: ProjectColors.textColorGrey, fontSize: 16),
+      textAlign: TextAlign.start,
+    );
+  }
+}
+
+class _coordinates extends StatelessWidget {
+  const _coordinates({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Column(
+            children: [
+              _PlaceTitles(
+                titleText: ProjectStrings.latitude,
+              ),
+              _textInput(),
+            ],
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              _PlaceTitles(
+                titleText: ProjectStrings.longitude,
+              ),
+              _textInput(),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _AddPlaceButton extends StatelessWidget {
+  const _AddPlaceButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 48,
+      child: MaterialButton(
+        materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
+        color: ProjectColors.textColorGrey,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const Text(
+              ProjectStrings.create,
+              style: TextStyle(color: Colors.white, fontSize: 20),
+            ),
+          ],
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+}
+
+class _textInput extends StatelessWidget {
+  final int lines;
+
+  const _textInput({this.lines = 1, Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        hintText: ProjectStrings.hint,
+        hintStyle:
+            TextStyle(fontSize: 16.0, color: ProjectColors.textColorGrey),
+        contentPadding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+      ),
+      minLines: lines,
+      maxLines: lines,
     );
   }
 }
